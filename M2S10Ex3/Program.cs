01d2b1c20643acfb425862f7d98e59c4e629f004
FHBaroni <86305650+FHBaroni@users.noreply.github.com>
2022-08-07 20:27:20 -0300

    M2S10Ex3

---
+++ M2S10Ex3/Program.cs
@@ -0,0 +1,22 @@
+﻿void ExemploGet()
+{
+    Console.WriteLine("Exemplo utilização http client - verbo GET - Deck of Cards api");
+
+    using var client = new HttpClient();
+
+    var request = new HttpRequestMessage(
+        HttpMethod.Get,
+        "http://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1"
+    );
+
+    Console.WriteLine("-- Requisição get");
+
+    using var response = client.Send(request);
+
+    Console.WriteLine($"Retorno: ");
+    Console.WriteLine($"Status Code: {response.StatusCode}");
+    Console.WriteLine($"Corpo: {response.Content.ReadAsStringAsync().Result}");
+    Console.WriteLine($"Headers: {string.Join(',', response.Headers.Select(h => $"{h.Key}={string.Join(',', h.Value)}"))}");
+}
+
+ExemploGet();
