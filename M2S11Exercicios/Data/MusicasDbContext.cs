13c31607d28247804bf2a1417909ab08f4a6434e
FHBaroni <86305650+FHBaroni@users.noreply.github.com>
2022-08-15 16:23:42 -0300

    M2S11Ex5

--- M2S11Exercicios/Data/MusicasDbContext.cs
+++ M2S11Exercicios/Data/MusicasDbContext.cs
@@ -10,6 +10,7 @@ namespace M2S11Exercicios.Data
         public MusicasDbContext(IConfiguration configuration)
         {
             _configuration = configuration;
+
         }
 
         public DbSet<Artista> Artistas { get; set; }
@@ -20,6 +21,85 @@ namespace M2S11Exercicios.Data
         {
             base.OnConfiguring(optionsBuilder);
             optionsBuilder.UseSqlServer(_configuration.GetConnectionString("DB_MUSICAS"));
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Artista>(entidade =>
+            {
+                entidade.ToTable("Artistas");
+
+                entidade.HasKey(a => a.Id);
+
+                entidade
+                .Property(a => a.Nome)
+                .HasMaxLength(120)
+                .IsRequired();
+
+                entidade
+                .Property(a => a.NomeArtistico)
+                .HasMaxLength(120);
+
+                entidade
+                .Property(a => a.FotoUrl)
+                .HasMaxLength(80)
+                .IsRequired();
+            });
+
+            modelBuilder.Entity<Album>(entidade =>
+            {
+                entidade.ToTable("Albuns");
+
+                entidade.HasKey(a => a.Id);
+
+                entidade
+                .Property(a => a.Nome)
+                .HasMaxLength(120)
+                .IsRequired();
+
+                entidade
+                .Property(a => a.AnoLancamento)
+                .IsRequired();
+
+                entidade
+                .Property(a => a.CapaUrl)
+                .HasMaxLength(80);
+
+
+                entidade
+                .HasOne(album => album.Artista)
+                .WithMany(artista => artista.Album)
+                .HasForeignKey(album => album.ArtistaId);
+
+            });
+
+            modelBuilder.Entity<Musica>(entidade =>
+            {
+                entidade.ToTable("Musicas");
+
+                entidade.HasKey(m => m.Id);
+
+                entidade
+                .Property(m => m.Nome)
+                .HasMaxLength(120)
+                .IsRequired();
+
+                entidade
+                .Property(m => m.Duracao)
+                .IsRequired();
+                               
+                entidade
+                .HasOne(musica => musica.Artista)
+                .WithMany(artista => artista.Musicas)
+                .HasForeignKey(musica => musica.ArtistaId);
+
+                entidade
+                .HasOne(musica => musica.Album)
+                .WithMany(album => album.Musicas)
+                .HasForeignKey(musica => musica.AlbumId);
+            });
         }
     }
 }
